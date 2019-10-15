dc9d83bbe2ee7015c73d25aa127f9d1250f296d0
catherine <zhangjiecatherine@gmail.com>
2019-10-15 08:56:17 -0400

    Pass data from 1st page to 2 page

--- app/src/main/java/android/example/twoactivities/SecondActivity.java
+++ app/src/main/java/android/example/twoactivities/SecondActivity.java
@@ -2,7 +2,9 @@ package android.example.twoactivities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        Intent intent = getIntent();
+        String message =
+                intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView = (TextView) findViewById(R.id.text_message);
+        textView.setText(message);
+
+
+
     }
 }
