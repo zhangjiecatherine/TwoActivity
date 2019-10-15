a467c428483206207edaaf20c8a841d50ef615c9
catherine <zhangjiecatherine@gmail.com>
2019-10-15 10:04:15 -0400

    Last step-reply info show in 1st page

--- app/src/main/java/android/example/twoactivities/MainActivity.java
+++ app/src/main/java/android/example/twoactivities/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,7 +15,12 @@ public class MainActivity extends AppCompatActivity {
 
     public static final String EXTRA_MESSAGE =
             "com.example.android.twoactivities.extra.MESSAGE";
+
+    public static final int TEXT_REQUEST = 1;
+
     private EditText mMessageEditText;
+    private TextView mReplyHeadTextView;
+    private TextView mReplyTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +28,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mMessageEditText = (EditText) findViewById(R.id.editText_main);
+        mReplyHeadTextView = (TextView) findViewById(R.id.text_header_reply);
+        mReplyTextView = (TextView) findViewById(R.id.text_message_reply);
     }
 
     public void launchSecondActivity(View view) {
@@ -33,6 +41,24 @@ public class MainActivity extends AppCompatActivity {
         String message = mMessageEditText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
 
-        startActivity(intent);
+        startActivityForResult(intent, TEXT_REQUEST);
     }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == TEXT_REQUEST) {
+            if (resultCode == RESULT_OK) {
+
+                String reply =
+                        data.getStringExtra(SecondActivity.EXTRA_REPLY);
+                mReplyHeadTextView.setVisibility(View.VISIBLE);
+                mReplyTextView.setText(reply);
+                mReplyTextView.setVisibility(View.VISIBLE);
+
+            }
+        }
+    }
+
 }
