44692d6395e00b2a86fc0da220852912208d659a
catherine <zhangjiecatherine@gmail.com>
2019-10-15 08:51:05 -0400

    Add intent

--- app/src/main/java/android/example/twoactivities/MainActivity.java
+++ app/src/main/java/android/example/twoactivities/MainActivity.java
@@ -6,15 +6,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
 
+    public static final String EXTRA_MESSAGE =
+            "com.example.android.twoactivities.extra.MESSAGE";
+    private EditText mMessageEditText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mMessageEditText = (EditText) findViewById(R.id.editText_main);
     }
 
     public void launchSecondActivity(View view) {
@@ -22,6 +29,10 @@ public class MainActivity extends AppCompatActivity {
         Log.d(LOG_TAG, "Button Clicked!");
 
         Intent intent = new Intent(this, SecondActivity.class);
+
+        String message = mMessageEditText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+
         startActivity(intent);
     }
 }
